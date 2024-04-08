176ab48ba5134e8aa88e3b87e8094f992c967499
Noah Arden <na3215987@gmail.com>
2024-04-08 18:18:57 +0000

    improved item component

--- src/components/StoreItem.vue
+++ src/components/StoreItem.vue
@@ -2,23 +2,41 @@
   <v-card
     variant="outlined"
     hover
+    
     :title="product.name"
-    :subtitle="ratingStr"
+    
     :text="product.description"
     :prepend-avatar="product.image"
   >
+    <template v-slot:subtitle >
+      <span class="d-flex justify-space-between">
+        <v-rating
+          v-model="product.rating"
+          active-color="yellow-accent-4"
+          color="black"
+          size="18"
+          half-increments
+          hover
+          readonly
+        ></v-rating>
+        <!-- <v-spacer/> -->
+        <span >${{ product.price.toFixed(2) }}</span>
+      </span>
+      
+      
+    </template>
   </v-card>
 </template>
 
 <script lang="ts" setup>
-import { ref } from "vue";
+//import { ref } from "vue";
 import { ProductDoc } from "../types/product";
 type Props = {
   product: ProductDoc;
 }
 const props = defineProps<Props>();
 const product = props.product.data;
-const ratingStr = ref(product.rating + "/5")
+//const ratingStr = ref(product.rating + "/5")
 //console.log(product);
 
 
